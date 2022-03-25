e7cfb41b8535f79d8fa76f90e3e93548100011a8
PrograminDR <96454918+PrograminDR@users.noreply.github.com>
2022-03-25 14:10:41 -0400

    c

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent{
+
+  img1='../assets/images/netflix.png';
+  img2="../assets/images/transmitir.png";
+  img3="../assets/images/buscar.png";
+  img4="../assets/images/filtrar.png";
+  img5="../assets/images/user.png";
+
+}
