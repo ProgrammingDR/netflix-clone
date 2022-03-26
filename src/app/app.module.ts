6473bde5bed962adf340acc1c6f6f71703302d9a
PrograminDR <96454918+PrograminDR@users.noreply.github.com>
2022-03-26 18:27:42 -0400

    q

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TitleComponent } from './components/title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/title/title.component.ts
@@ -0,0 +1,15 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css']
+})
+export class TitleComponent {
+
+  img1= "../assets/images/netflix.png";
+  img2= "../assets/images/mas.png";
+  img3= "../assets/images/boton-de-informacion.png";
+  img4= "../assets/images/jugar-buttton.png";
+
+}
