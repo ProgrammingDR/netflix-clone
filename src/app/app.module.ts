0d007e6b36ccde330a19e4dda8fb5d4e8de88dac
programindr <alexithoswagger15@gmail.com>
2022-03-26 20:48:30 -0400

    4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TitleComponent } from './components/title/title.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TitleComponent
+    TitleComponent,
+    PeliculasComponent
   ],
   imports: [
     BrowserModule,
